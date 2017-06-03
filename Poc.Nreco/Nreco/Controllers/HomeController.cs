549813a3512535fe45ee16ed2dcd1737474d1191
Gumiz <micgrn@gmail.com>
2017-06-03 14:41:40 +0200

    NReco POC Initial

---
+++ Poc.Nreco/Nreco/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net.Mime;
+using System.Web.Mvc;
+
+namespace Nreco.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		public ActionResult Print()
+		{
+			return View();
+		}
+
+		public FileContentResult GetPdf()
+		{
+			var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
+			var pdfBytes = htmlToPdf.GeneratePdf(GetSamplePdfString(200));
+			return File(pdfBytes, "application/pdf", "NRecoTestFile.pdf");
+		}
+
+		private string GetSamplePdfString(int numberOfPages = 10)
+		{
+			var result = GetSampleHtmlSchema();
+			var rows = "";
+			for (var i = 0; i < numberOfPages * 20; i++)
+				rows += GetSampleRow(i);
+			return result.Replace("{row}", rows);
+		}
+
+		private string GetSampleHtmlSchema()
+		{
+			return @"<html>
+<head>
+	<meta http-equiv='content-type' content='text/html; charset=utf-8' />
+	<style>table { border:1px solid silver; border-collapse:collapse;} table td { border-bottom:1px solid silver; }</style>
+</head>
+	<body>
+		<h1 style='text-align:center;'>Order #23</h1>
+		<div style='float:left;'>
+			Donald<br />
+			47338 Park Avenue<br />
+			Big City<br /><br />
+			Date: 6/3/2017
+		</div>
+		<div style='float:right;'>
+			<div style='border:1px solid gray;padding:10px;'>
+				A company<br />
+				321 City Street<br />
+				Industry Park<br />
+				Another Country
+			</div>
+		</div>
+		<div style='clear:both;'></div>
+		<br /><br />
+		Ordered items list:
+		<table width='100%' style='margin-bottom:20px;'>
+			{row}
+		</table>
+
+
+		<div style='float:right;'>
+			Signed: Saturday, June 3, 2017 12:06 PM<br />
+			From IP: 31.134.60.75
+			Signature:<br />
+			<img src='https://www.nrecosite.com/img/pdfgenerator/donald_sign.jpg'/>
+		</div>
+		<div style='float:left;'>Sincerely,<br />Donald</div>
+		<div style='clear:both;'></div>
+
+	</body>
+</html>";
+		}
+
+		private string GetSampleRow(int rowNumber)
+		{
+			return $@"<tr>
+				<td>{rowNumber:0000000}</td>
+				<td><b>Sample name</b></td>
+				<td><i>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui</i></td>
+			</tr>";
+		}
+	}
+}
---
+++ Poc.Nreco/Nreco/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nreco.Startup))]
+namespace Nreco
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
